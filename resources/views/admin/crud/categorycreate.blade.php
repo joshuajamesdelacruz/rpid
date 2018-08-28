28810560812ebb6013382099a0e7057e55c217c8
Joshua James <joshuajamesdelacruz@users.noreply.github.com>
2018-08-28 14:18:04 +0800

    test
    test

--- resources/views/admin/crud/categorycreate.blade.php
+++ resources/views/admin/crud/categorycreate.blade.php
@@ -1,12 +1,15 @@
 @extends('admin.crud.master')
 @section('content')
 
+<div class="container"> 
+
 <h3>Create</h3>
 <form method="POST" action="{{ url('category') }}" >
          {{ csrf_field() }}
     <input type="text" name="category">
     <input type="submit" class="btn btn-primary">
-    <input type="submit" class="btn btn-danger" value="cancel">
+    <input type="cancel" class="btn btn-danger" value="cancel">
 </form>
+</div> 
 
 @endsection
--- resources/views/admin/crud/categoryedit.blade.php
+++ resources/views/admin/crud/categoryedit.blade.php
@@ -11,7 +11,7 @@
         <input type="text" class="form-control form-control-lg" id="lgFormGroupInput" name="category" value="{{ $ItemCode->category }}">
     </div>
     <input type="submit" class="btn btn-primary">
-    <input type="submit" class="btn btn-danger" value="cancel">
+    <input type="cancel" class="btn btn-danger" value="cancel">
 </form>
    
 @endsection
