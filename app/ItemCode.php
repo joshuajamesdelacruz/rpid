9eb8fc616593a61a77875c85f3f8bace0887e6e6
Joshua James <joshuajamesdelacruz@users.noreply.github.com>
2018-10-23 13:52:45 +0800

    Update app/ItemCode.php

--- app/ItemCode.php
+++ app/ItemCode.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class ItemCode extends Model
 {
+    use Searchable;
     protected $fillable = ['category'];
+
+
+    public function toSearchableArray()
+    {
+    $array = $this->toArray();
+        
+    return array('id' => $array['id'],'category' => $array['category']);
+    }
+
 }
